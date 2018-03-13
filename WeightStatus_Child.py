1e2d2e8d38d1f8a8884ea53b091a4cc6d3690994
msholes06 <msholes06@yahoo.com>
2018-03-12 21:31:33 -0400

    Add files via upload
    Updated

--- WeightStatus_Child.py
+++ WeightStatus_Child.py
@@ -5,6 +5,11 @@ print("This is a program that calculates a child's 'Body Mass Index' based on ge
 print("Based on a child's BMI, we are able to determine within which national percentile range\nthe child falls and their respective 'Weight Status Category.'")
 
 ##print("Weight Status Categories are defined as either 'Underweight', 'Normal',\n'Healthy Weight', 'Overweight', or 'Obese'.")
+##child_age = int(input("Please enter the child's age 2-5 (whole years only): ")
+ ## height = int(input("Please enter the child's height in ft: "))
+## child_weight = input("Please enter the child's weight in pounds (lbs): ")
+##gender = child_gender(input("Please enter the child's gender (1 for Male or 2 for Female): "))
+
 
     
     
@@ -17,7 +22,10 @@ print("Based on a child's BMI, we are able to determine within which national pe
     
 
 while True:
-    
+
+    child_weight = float(input("Please enter the child's weight in pounds (lbs): "))
+    child_age = int(input("Please enter the child's age 2-5 (whole years only): "))
+
     def convert_to_inches(ft):
         '''(number) -> number
         Converts feet to inches.
@@ -29,7 +37,7 @@ while True:
         child_in = ft * 12
         return child_in
 
-##        child_ht = convert_to_inches(float(input("Please enter the child's height in ft: ")))
+    child_ht = convert_to_inches(float(input("Please enter the child's height in ft: ")))
     
     def child_gender(gender):
 ##        gender = child_gender(input("Please enter the child's gender (1 for Male or 2 for Female): "))
@@ -39,7 +47,8 @@ while True:
             s = 2
         else:
             print("Please enter 1 for Male or 2 for Female: ")
-            
+
+    gender = child_gender(input("Please enter the child's gender (1 for Male or 2 for Female): "))           
  
     
     def body_mass_index(height, weight):
@@ -55,18 +64,19 @@ while True:
            '''
         return ((weight / height ** 2) * 703)
     
-    child_age = int(input("Please enter the child's age 2-5 (whole years only): ")
-    child_ht = convert_to_inches(float(input("Please enter the child's height in ft: ")))
-    child_weight = input("Please enter the child's weight in pounds (lbs): ")
-    gender = child_gender(input("Please enter the child's gender (1 for Male or 2 for Female): "))
-
+##    child_age = int(input("Please enter the child's age 2-5 (whole years only): ")
+##    child_ht = convert_to_inches(float(input("Please enter the child's height in ft: ")))
+##    child_weight = input("Please enter the child's weight in pounds (lbs): ")
+##    gender = child_gender(input("Please enter the child's gender (1 for Male or 2 for Female): "))
+##
     bmi = body_mass_index(child_ht, child_weight)
+                    
     a = child_age
+    s = gender
 
-    print("Your child's bmi is ", bmi,")
-
+    print("Your child's bmi is ", bmi)
 ## Health Status Male (2-5 yo)
-          
+    
     if bmi < 14.7 and a == 2 and s == 1:
           print("Your child is UNDERWEIGHT for their sex and age.")
           print("UNDERWEIGHT children are more likely to face health problems.")
@@ -83,7 +93,7 @@ while True:
           print("Your child is OBESE for their sex and age.")
           print("Children who are OBESE are more likely to face health problems.")
           print("Please see a doctor or seek medical advice.")
-    elif bmi < 14.4 and a == 3 and s == 1
+    elif bmi < 14.4 and a == 3 and s == 1:
           print("Your child is UNDERWEIGHT for their sex and age.")
           print("UNDERWEIGHT children are more likely to face health problems.")
           print("Please see a doctor or seek medical advice.")
@@ -132,11 +142,92 @@ while True:
           print("Children who are OBESE are more likely to face health problems.")
           print("Please see a doctor or seek medical advice.")
           
+## Health Status Female (2-5 yo)
+
+    elif bmi < 14.4 and a == 2 and s == 2:
+          print("Your child is UNDERWEIGHT for their sex and age.")
+          print("UNDERWEIGHT children are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif 14.4 <= bmi < 18.0 and a == 2 and s == 2:
+          print("Your child is NORMAL or HEALTHY WEIGHT for their sex and age.")
+          print("Children who are NORMAL or HEALTY WEIGHT are less likely to face health problems.")
+          print("However, do NOT neglect to take your child to their doctor regulary.")
+    elif 18.0 <= bmi < 19.1 and a == 2 and s == 2:
+          print("Your child is OVERWEIGHT for their sex and age.")
+          print("Children who are OVERWEIGHT are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi >= 19.1 and a == 2 and s == 2:
+          print("Your child is OBESE for their sex and age.")
+          print("Children who are OBESE are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi < 14.0 and a == 3 and s == 2:
+          print("Your child is UNDERWEIGHT for their sex and age.")
+          print("UNDERWEIGHT children are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif 14.0 <= bmi < 17.2 and a == 3 and s == 2:
+          print("Your child is NORMAL or HEALTHY WEIGHT for their sex and age.")
+          print("Children who are NORMAL or HEALTY WEIGHT are less likely to face health problems.")
+          print("However, do NOT neglect to take your child to their doctor regulary.")
+    elif 17.2 <= bmi < 18.3 and a == 3 and s == 2:
+          print("Your child is OVERWEIGHT for their sex and age.")
+          print("Children who are OVERWEIGHT are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi >= 18.3 and a == 3 and s == 2:
+          print("Your child is OBESE for their sex and age.")
+          print("Children who are OBESE are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi <= 13.8 and a == 4 and s == 2:
+          print("Your child is UNDERWEIGHT for their sex and age.")
+          print("UNDERWEIGHT children are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif 13.8 <= bmi < 16.8 and a == 4 and s == 2:
+          print("Your child is NORMAL or HEALTHY WEIGHT for their sex and age.")
+          print("Children who are NORMAL or HEALTY WEIGHT are less likely to face health problems.")
+          print("However, do NOT neglect to take your child to their doctor regulary.")
+    elif 16.8 <= bmi < 18.0 and a == 4 and s == 2:
+          print("Your child is OVERWEIGHT for their sex and age.")
+          print("Children who are OVERWEIGHT are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi >= 18.0 and a == 4 and s == 2:
+          print("Your child is OBESE for their sex and age.")
+          print("Children who are OBESE are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi <= 13.6 and a == 5 and s == 2:
+          print("Your child is UNDERWEIGHT for their sex and age.")
+          print("UNDERWEIGHT children are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif 13.6 <= bmi < 16.8 and a == 5 and s == 2:
+          print("Your child is NORMAL or HEALTHY WEIGHT for their sex and age.")
+          print("Children who are NORMAL or HEALTY WEIGHT are less likely to face health problems.")
+          print("However, do NOT neglect to take your child to their doctor regulary.")
+    elif 16.8 <= bmi < 18.2 and a == 5 and s == 2:
+          print("Your child is OVERWEIGHT for their sex and age.")
+          print("Children who are OVERWEIGHT are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
+    elif bmi >= 18.2 and a == 5 and s == 2:
+          print("Your child is OBESE for their sex and age.")
+          print("Children who are OBESE are more likely to face health problems.")
+          print("Please see a doctor or seek medical advice.")
           
-    
 
           
 
+          
+
+          
+
+          
+          
+          
+
+          
+
+          
+
+          
+
+            
+          
           
 
           
